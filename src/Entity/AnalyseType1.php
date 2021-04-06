f133d61c8d2512b8d102cbea81e14f6d2283cc00
ala <alasafi72@gmail.com>
2021-04-06 17:24:17 +0100

    first commit

--- src/Entity/AnalyseType1.php
+++ src/Entity/AnalyseType1.php
@@ -2,9 +2,13 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\AnalyseType1Repository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
 
 /**
  * @ApiResource()
